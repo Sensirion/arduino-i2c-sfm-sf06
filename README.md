c3844c71a499a393f01db9d8076afd605c10281f
Rolf Laich <rolf.laich@sensirion.com>
2021-10-25 12:05:23 +0200

    Generated files Checking of files generated by driver generator without modifications
    jetzt noch mit versteckten dateien

--- README.md
+++ README.md
@@ -1,3 +1,84 @@
-# arduino I2C sfm-sf06
+# Sensirion I2C SFM_SF06 Arduino Library
 
-Arduino driver for the sensor familiy sfm_sf06
+This is the Sensirion SFM_SF06 library for Arduino using the
+modules I2C interface.
+
+TODO: DRIVER_GENERATOR Add image of sensor / eval kit as images/sfm-sf06.jpg
+[<center><img src="images/SFM-SF06.png" width="300px"></center>](TODO: DRIVER_GENERATOR add url to the eval kit)
+
+Click [here](TODO: DRIVER_GENERATOR Add url to the eval kit) to learn more about the SFM_SF06
+sensor and the SFM_SF06 Evaluation Kit Board.
+
+
+# Installation
+
+To install, download the latest release as .zip file and add it to your
+[Arduino IDE](http://www.arduino.cc/en/main/software) via
+
+	Sketch => Include Library => Add .ZIP Library...
+
+Don't forget to **install the dependencies** listed below the same way via `Add
+.ZIP Library`
+
+Note: Installation via the Arduino Library Manager is coming soon.
+
+# Dependencies
+
+* [Sensirion Core](https://github.com/Sensirion/arduino-core)
+
+
+# Quick Start
+
+1. Connect the SFM_SF06 Sensor to your Arduino board's standard
+   I2C bus. Check the pinout of your Arduino board to find the correct pins.
+   The pinout of the SFM_SF06 Sensor board can be found in the
+   data sheet.
+
+   | *SFM_SF06* |   *Arduino*    | *Jumper Wire* |
+   |---------|----------------|---------------|
+   |   VDD   |       xV       |     Red       |  TODO: DRIVER_GENERATOR Add correct voltage
+   |   GND   |       GND      |     Black     |
+   |   SDA   |       SDA      |     Green     |
+   |   SCL   |       SCL      |     Yellow    |
+   |   SEL   |   GND for I2C  |     Blue      |  TODO: DRIVER_GENERATOR Remove if not available for your sensor
+
+
+2. Open the `exampleUsage` sample project within the Arduino IDE
+
+		File => Examples => Sensirion I2C SFM_SF06 => exampleUsage
+
+3. Click the `Upload` button in the Arduino IDE or
+
+		Sketch => Upload
+
+4. When the upload process has finished, open the `Serial Monitor` or `Serial
+   Plotter` via the `Tools` menu to observe the measurement values. Note that
+   the `Baud Rate` in the corresponding window has to be set to `115200 baud`.
+
+# Contributing
+
+**Contributions are welcome!**
+
+We develop and test this driver using our company internal tools (version
+control, continuous integration, code review etc.) and automatically
+synchronize the master branch with GitHub. But this doesn't mean that we don't
+respond to issues or don't accept pull requests on GitHub. In fact, you're very
+welcome to open issues or create pull requests :)
+
+This Sensirion library uses
+[`clang-format`](https://releases.llvm.org/download.html) to standardize the
+formatting of all our `.cpp` and `.h` files. Make sure your contributions are
+formatted accordingly:
+
+The `-i` flag will apply the format changes to the files listed.
+
+```bash
+clang-format -i src/*.cpp src/*.h
+```
+
+Note that differences from this formatting will result in a failed build until
+they are fixed.
+
+# License
+
+See [LICENSE](LICENSE).
---
+++ examples/exampleUsage/exampleUsage.ino
@@ -0,0 +1,105 @@
+/*
+ * I2C-Generator: 0.3.0
+ * Yaml Version: 1.1.0
+ * Template Version: 0.7.0-78-g11fb280
+ */
+/*
+ * Copyright (c) 2021, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <Arduino.h>
+#include <SensirionI2CSfmSf06.h>
+#include <Wire.h>
+
+SensirionI2CSfmSf06 sfmSf06;
+
+// TODO: DRIVER_GENERATOR Add missing commands and make printout more pretty
+
+void setup() {
+
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(100);
+    }
+
+    Wire.begin();
+
+    uint16_t error;
+    char errorMessage[256];
+
+    sfmSf06.begin(Wire);
+
+    error = sfmSf06.stopContinuousMeasurement();
+
+    if (error) {
+        Serial.print("Error trying to execute stopContinuousMeasurement(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
+
+    uint32_t productIdentifier;
+    uint8_t serialNumber[8];
+    uint8_t serialNumberSize = 8;
+
+    error = sfmSf06.readProductIdentifier(productIdentifier, serialNumber,
+                                          serialNumberSize);
+
+    if (error) {
+        Serial.print("Error trying to execute readProductIdentifier(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("ProductIdentifier:");
+        Serial.print(productIdentifier);
+        Serial.print("\t");
+        Serial.print("SerialNumber:");
+        Serial.print("0x");
+        for (size_t i = 0; i < serialNumberSize; i++) {
+            uint16_t value = serialNumber[i];
+            Serial.print(value < 4096 ? "0" : "");
+            Serial.print(value < 256 ? "0" : "");
+            Serial.print(value < 16 ? "0" : "");
+            Serial.print(value, HEX);
+        }
+        Serial.println();
+    }
+
+    // Start Measurement
+}
+
+void loop() {
+    uint16_t error;
+    char errorMessage[256];
+
+    // TODO: DRIVER_GENERATOR Adjust measurement delay
+    delay(1000);
+    // TODO: DRIVER_GENERATOR Add scale and offset to printed measurement values
+    // Read Measurement
+}
